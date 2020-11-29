106bd2d1544151bb6bd63fb21c2644e039ff131a
kenorb <kenorb@users.noreply.github.com>
2020-11-29 21:59:00 +0000

    Adds strategy files

---
+++ config/EURUSD_M5.h
@@ -0,0 +1,23 @@
+/*
+ * @file
+ * Defines strategy's default parameter values
+ * for the given pair symbol and timeframe.
+ */
+
+// Defines strategy's parameter values for the given pair symbol and timeframe.
+struct Stg_eSAWA_Params_M5 : StgParams {
+  // Struct constructor.
+  Stg_eSAWA_Params_M5() : StgParams(stg_esawa_defaults) {
+    lot_size = 0;
+    signal_open_method = 0;
+    signal_open_filter = 1;
+    signal_open_level = 0;
+    signal_open_boost = 0;
+    signal_close_method = 0;
+    signal_close_level = 0;
+    price_stop_method = 0;
+    price_stop_level = 2;
+    tick_filter_method = 1;
+    max_spread = 0;
+  }
+} stg_esawa_m5;
